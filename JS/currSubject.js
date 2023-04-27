37e8766904c3677201078d384766fca90c505197
IMREYZ <m-sm79@mail.ru>
2023-04-27 13:10:54 +0300

    1

--- JS/currSubject.js
+++ JS/currSubject.js
@@ -2,152 +2,63 @@
 if (window.location.pathname === `/MathWeb/HTML/currSubject.html`){
 
     // Считываем номер задания и имя задания
-    const id = getLocalStorage('idProblem')
-    const name = getLocalStorage('nameProblem')
+    const id = getLocalStorage('idProblem') // Номер задания
+    const name = getLocalStorage('nameProblem') // Имя задания
+    const title = document.querySelector('.title') // Заголовок
+    const allConteynerProblem = document.querySelector('.allConteynerPr') // Имя задачи
+    const nameProblem = document.querySelector('.name') // Контейнер html разметки
+    const allProblems = problems[id] // Все задачи
 
-    // Заголовок
-    const title = document.querySelector('.title')
-    title.innerHTML = `${name}`
-
-    
-    // Имя задачи и контейнер html разметки
-    const allConteynerProblem = document.querySelector('.allConteynerPr')
-    const nameProblem = document.querySelector('.name')
-     
-
-    // Устанавливаем название; все задачи
     nameProblem.innerHTML = name
-    let allProblems = problems[id]
+    title.innerHTML = name
+    
+    if (!getLocalStorage('thisProblems')) setLocalStorage('thisProblems', allProblems) // Если в LocalStr нет текущих задач - добавляем все задачи
+    if (!getLocalStorage('select')) setLocalStorage('select', 'oldToNew') // Если в LocalStr нет select - oldToNew
 
-
-    // Если в LocalStr нет текущих задач - добавляем все задачи
-    if (!getLocalStorage('thisProblems')) setLocalStorage('thisProblems', allProblems)
-    if (!getLocalStorage('select')) setLocalStorage('select', 'oldToNew')
 
 
     // Добавляем Checkbox на страницу
-    const filterConteyner = document.querySelector('.fil')
-    const themeThisProblem = themeProblems[id]
-    let thisTypes = []
+    const filterConteyner = document.querySelector('.fil') // Контейнер
+    const themeThisProblem = themeProblems[id] // Темы задачи
+    let thisTypes = [] // Массив всех тем
     for (let index = 0; index < themeThisProblem.length; index ++) thisTypes.push(themeThisProblem[index].name)
 
     let text = ``
-    themeThisProblem.forEach(element => text += `<div class='SSS'> <input class='checkbox' type='checkbox' id='${getNormalClass(element.name)}'> <span class='${getNormalClass(element.name)}'>${element.name} (${element.count} шт.)</span> </div>`)
-    filterConteyner.innerHTML += text
-
-    // Все checkbox в HTML
-    const allCheckbox = document.querySelectorAll('.checkbox')
-
-    const minusBox = document.querySelector('.minusBox')
-    const plusBox = document.querySelector('.plusBox')
-
-    
-
-    // Если в LocalStr был checkbox  (в случае обновления страницы)
-    if (getLocalStorage('checkbox')){
-        // Массив checkbox
-        const arrayCheckbox = getLocalStorage('checkbox')
-
-        // Проходимся по всем checkbox HTML
-        allCheckbox.forEach(element => {
-            // Если id in arrayCheckbox, то
-           
-            
-            if (arrayCheckbox.includes(element.id)) {
-
-                // Делаем checked = true; добавляем w900
-                element.checked = true
-                addW900(`.${getNormalClass(element.id)}`, true)
-            }
-        })
-        
-    // Если не было в LocalStr
-    } else {
-        // Проходимся по всем checkbox HTML
-        allCheckbox.forEach(element => {
-            // Ставим всем w900 и checked
-            element.checked = true 
-            addW900(`.${getNormalClass(element.id)}`, true)
-        })
-    }
-
-
-    
-    // Обработка события нажатия на checkbox
-    document.addEventListener('click', (event) => {
-        // Если не checkbox - выходим
-        if (event.target.classList[0] != 'checkbox') return
-
-        // Если checkbox нет в LocalStr - пустой массив
-        if (!getLocalStorage('checkbox')) setLocalStorage('checkbox', [])
-
-        // Массив значений checkbox
-        let checkboxArray = []
-
-        // Проходимся по всем checkbox HTML
-        allCheckbox.forEach(element => {
-            // Если checked
-            if (element.checked) {
-                // Добавляем в массив id; добавляем w900
-                checkboxArray.push(element.id)
-                
-
-                addW900(`.${getNormalClass(element.id)}`, true)
-
-            // Иначе убираем w900
-            } else {
-                addW900(`.${getNormalClass(element.id)}`, false)
-            }
-        })
-        
-
-        // Устанавлием checkbox в LocalStr
-        setLocalStorage('checkbox', checkboxArray)
-
-        // Массив новых задач
-        let newThisProblems = []
-
-        // Проходимся по базовому массиву всех задач
-        for (let i = 0; i < allProblems.length; i ++){
-            // Если type задачи in checkboxArray: добавляем задачу в новый массив
-            if (checkboxArray.includes(getNormalClass(allProblems[i].type))) newThisProblems.push(allProblems[i])
-        }
-        //console.log(newThisProblems, 1)
-        
-
-        // Обновляем текущие задачи; сортируем эти задачи\     
-        setLocalStorage('thisProblems', newThisProblems)
-        
-        sortProblem(getLocalStorage('select'))
-    
-        // Выводим задания
-        allConteynerProblem.innerHTML = ''
-        getThisProblems().forEach(element => allConteynerProblem.innerHTML += problemHTMLcurr(element))
-
-        // Возвращаем цвета; инпуты; инфу об "показать ответ"
-        currColor(getThisProblems())
-        currInput(getThisProblems())
-        currInfoShowRightAnswer(getThisProblems())
+    themeThisProblem.forEach(element => {
+        const nameNormalClass = getNormalClass(element.name)
+        text += `<div class='SSS'> 
+                    <input class='checkbox' type='checkbox' id='${nameNormalClass}'> 
+                    <span class='${nameNormalClass}'>${element.name} (${element.count} шт.)</span> 
+                 </div>`
     })
 
+    filterConteyner.innerHTML += text
+
+    const allCheckbox = document.querySelectorAll('.checkbox') // Все checkbox в HTML
+    const minusBox = document.querySelector('.minusBox') // Убрать всё
+    const plusBox = document.querySelector('.plusBox') // Добавить всё
 
 
-    // Забираем из LocalStr select; текущий select
-    const thisSelect = getLocalStorage('select')
-    const optionCurr = document.querySelector('.optionCurr')
 
-    // Если есть select
-    if (thisSelect) {
-        // Сортировка к изначальному виду
-        sortProblem(thisSelect)
+    if (getLocalStorage('checkbox')){ // Если в LocalStr был checkbox  (в случае обновления страницы)
+        const arrayCheckbox = getLocalStorage('checkbox') // Массив checkbox
 
-        // Возвращаем к тому, что было до обновления
-        optionCurr.querySelector(`[value=${thisSelect}]`).selected="selected"
+        allCheckbox.forEach(element => { // Проходимся по всем checkbox HTML        
+            if (arrayCheckbox.includes(element.id)) { // Если id in arrayCheckbox, то
+                element.checked = true // Делаем checked = true
+                addW900(`.${element.id}`, true) // Добавляем w900
+            }
+        })
+        
+    } else { // Если не было в LocalStr
+        allCheckbox.forEach(element => { // Проходимся по всем checkbox HTML
+            element.checked = true // checked
+            addW900(`.${element.id}`, true) // w900
+        })
     }
 
     // Первый вывод заданий 
-    if (getLocalStorage('thisProblems')) getLocalStorage('thisProblems').forEach(element => allConteynerProblem.innerHTML += problemHTMLcurr(element))
-    else allProblems.forEach(element => allConteynerProblem.innerHTML += problemHTMLcurr(element))
+    getThisProblems().forEach(element => allConteynerProblem.innerHTML += problemHTMLcurr(element))
 
 
     // Возвращаем цвета; инпуты; инфу об "показать ответ"
@@ -155,100 +66,128 @@ if (window.location.pathname === `/MathWeb/HTML/currSubject.html`){
     currInput(getThisProblems())
     currInfoShowRightAnswer(getThisProblems())
 
-    // Обработка события "Сортировка"
-    optionCurr.addEventListener('change', () => {
-        // Текущий пик
-        const thisOption = optionCurr.value
+    
+    document.addEventListener('click', (event) => { // Обработка события нажатия на checkbox
+        if (event.target.classList[0] !== 'checkbox') return // Если не checkbox - выходим
+
+        if (!getLocalStorage('checkbox')) setLocalStorage('checkbox', []) // Если checkbox нет в LocalStr - пустой массив
+
+        const checkboxArray = [] // Массив значений checkbox
+
+        allCheckbox.forEach(element => { // Проходимся по всем checkbox HTML
+            if (element.checked) { // Если checked
+                checkboxArray.push(element.id) // Добавляем в массив id
+                addW900(`.${element.id}`, true) // Добавляем w900
+
+            } else addW900(`.${element.id}`, false) // Иначе убираем w900
+        })
         
-        // Сортируем по новому изменению
-        sortProblem(thisOption)
+        setLocalStorage('checkbox', checkboxArray) // Устанавлием checkbox в LocalStr
 
-        // Добавляем в LocalStr select
-        setLocalStorage('select', thisOption)
+        const newThisProblems = [] // Массив новых задач
 
-        // Образовываем обновленную страницу
+        for (let index = 0; index < allProblems.length; index ++){ // Проходимся по базовому массиву всех задач
+            const normalTypeThisProblem = getNormalClass(allProblems[index].type)
+
+            if (checkboxArray.includes(normalTypeThisProblem)) { // Если type задачи in checkboxArray: добавляем задачу в новый массив
+                newThisProblems.push(allProblems[index]) 
+            }
+        }
+        
+        setLocalStorage('thisProblems', newThisProblems) // Обновляем текущие задачи
+        sortProblem(getLocalStorage('select')) // сортируем эти задачи
+    
         allConteynerProblem.innerHTML = ''
-        getThisProblems().forEach(element => allConteynerProblem.innerHTML += problemHTMLcurr(element))
+        getThisProblems().forEach(element => allConteynerProblem.innerHTML += problemHTMLcurr(element)) // Выводим задания
 
-        // Возвращаем цвета и инпуты в случае инпута
+        currColor(getThisProblems()) // Выводим цвета
+        currInput(getThisProblems()) // Выводим input
+        currInfoShowRightAnswer(getThisProblems()) // Выводим "показать ответ"
+    })
+
+
+
+    const thisSelect = getLocalStorage('select') // Забираем из LocalStr select
+    const optionCurr = document.querySelector('.optionCurr') // Текущий select
+
+    if (thisSelect) { // Если есть select
+        sortProblem(thisSelect) // Сортировка к изначальному виду
+        optionCurr.querySelector(`[value=${thisSelect}]`).selected="selected" // Возвращаем к тому, что было до обновления
+    }
+
+
+    optionCurr.addEventListener('change', () => {  // Обработка события "Сортировка"
+        const thisOption = optionCurr.value // Текущий пик
+        
+        sortProblem(thisOption) // Сортируем по новому изменению
+
+        setLocalStorage('select', thisOption) // Добавляем в LocalStr select
+
+        allConteynerProblem.innerHTML = ''
+        getThisProblems().forEach(element => allConteynerProblem.innerHTML += problemHTMLcurr(element)) // Образовываем обновленную страницу
+
         currColor(getThisProblems())
         currInput(getThisProblems())
         currInfoShowRightAnswer(getThisProblems())
     })
 
-
+    
     // Нажатие на "Ответить"
     eventSendAnswer()
 
-    minusBox.addEventListener('click', () => {
-        // Если checkbox нет в LocalStr - пустой массив
-        if (!getLocalStorage('checkbox')) setLocalStorage('checkbox', [])
 
-        // Проходимся по всем checkbox HTML
-        allCheckbox.forEach(element => {
+    minusBox.addEventListener('click', () => { // Обработка события - нажатие на "удалить всё"
+        if (!getLocalStorage('checkbox')) setLocalStorage('checkbox', []) // Если checkbox нет в LocalStr - пустой массив
+
+        allCheckbox.forEach(element => { // Проходимся по всем checkbox HTML
             element.checked = false
             addW900(`.${element.id}`, false)
         })
 
-
-        // Обновляем текущие задачи; сортируем эти задачи
-        setLocalStorage('thisProblems', [])
+        setLocalStorage('thisProblems', []) // Текущие задачи = []
     
-        // Выводим задания
-        allConteynerProblem.innerHTML = ''
+        allConteynerProblem.innerHTML = '' // Выводим задания
 
-        setLocalStorage('checkbox', [])
+        setLocalStorage('checkbox', []) // checkbox = []
 
-        // Возвращаем цвета; инпуты; инфу об "показать ответ"
         currColor(getThisProblems())
         currInput(getThisProblems())
         currInfoShowRightAnswer(getThisProblems())
     })
 
-    plusBox.addEventListener('click', () => {
-        // Если checkbox нет в LocalStr - пустой массив
-        if (!getLocalStorage('checkbox')) setLocalStorage('checkbox', [])
+
+    plusBox.addEventListener('click', () => { // Обработка события - нажатие на "добавить всё"
+        if (!getLocalStorage('checkbox')) setLocalStorage('checkbox', []) // Если checkbox нет в LocalStr - пустой массив
 
         const fullBox = []
-        // Проходимся по всем checkbox HTML
-        allCheckbox.forEach(element => {
+        allCheckbox.forEach(element => { // Проходимся по всем checkbox HTML
             element.checked = true
             addW900(`.${element.id}`, true)
             fullBox.push(element.id)
         })
 
-        setLocalStorage('checkbox', fullBox)
+        setLocalStorage('checkbox', fullBox) // checkbox = все checkbox
 
-        // Обновляем текущие задачи; сортируем эти задачи
-        setLocalStorage('thisProblems', allProblems)
-        sortProblem(getLocalStorage('select'))
+        setLocalStorage('thisProblems', allProblems) // Все задачи
+        sortProblem(getLocalStorage('select')) // Сортируем
     
-        // Выводим задания
         allConteynerProblem.innerHTML = ''
-        getThisProblems().forEach(element => allConteynerProblem.innerHTML += problemHTMLcurr(element))
+        getThisProblems().forEach(element => allConteynerProblem.innerHTML += problemHTMLcurr(element)) // Выводим задания
 
-        // Возвращаем цвета; инпуты; инфу об "показать ответ"
         currColor(getThisProblems())
         currInput(getThisProblems())
         currInfoShowRightAnswer(getThisProblems())
     })
 
 
+    createAndSaveColors(getThisProblems()) // Сохранение
+    createAndSaveInfoAnswers(getThisProblems()) // Сохранение
+    createAndSaveInputs(getThisProblems()) // Сохранение
 
-    // Ограничение на input
-    document.addEventListener('input', (event) => {
 
-        // Если событие - не кнопка input, то выходим
-        if (event.target.classList[0] != 'input') return 
+    document.addEventListener('input', (event) => { // Ограничение на input
+        if (event.target.classList[0] !== 'input') return // Если событие - не кнопка input, то выходим
 
-        // Регулярное выражение для фильтрации input
-        event.target.value = event.target.value.replace(/[^0123456789,-]/g, '')
+        event.target.value = event.target.value.replace(/[^0123456789,-]/g, '') // Регулярное выражение для фильтрации input
     })
-    
-
-
-    // Создание и/или сохранение 1) цветов 2) "показать ответ"  3) input
-    createAndSaveColors(getThisProblems()) 
-    createAndSaveInfoAnswers(getThisProblems())
-    createAndSaveInputs(getThisProblems())
 }
